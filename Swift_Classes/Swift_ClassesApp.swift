e5412fba385c48167eda0235c9136154c9139acd
ZachMommaerts <zmommaerts@gmail.com>
2023-03-04 22:56:33 -0600

    Initial Commit

---
+++ Swift_Classes/Swift_ClassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ClassesApp.swift
+//  Swift_Classes
+//
+//  Created by Zach Mommaerts on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ClassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
